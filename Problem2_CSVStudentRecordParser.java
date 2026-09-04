6ef011702dbcd5b9953ff7336c0df88a10c4b9a8
Sonu kumar <sonukumarshk47@gmail.com>
2026-09-05 00:50:51 +0530

    Add Week 2 practice problems

---
+++ Problem2_CSVStudentRecordParser.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Problem2_CSVStudentRecordParser {
+
+    static void parseStudentRecord(String csvLine) {
+
+        String[] fields = csvLine.split(",");
+
+        if (fields.length != 3) {
+
+            System.out.println("Invalid Record");
+            return;
+        }
+
+        String name = fields[0].trim();
+        String rollNumber = fields[1].trim();
+        String department = fields[2].trim();
+
+        System.out.println(
+                "Name: " + name +
+                " | Roll No: " + rollNumber +
+                " | Dept: " + department
+        );
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter student record: ");
+        String csvLine = sc.nextLine();
+
+        parseStudentRecord(csvLine);
+
+        sc.close();
+    }
+}
